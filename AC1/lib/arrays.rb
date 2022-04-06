357f3e8333f52cea8ac9ae5365454f1bd59c4ddc
bryn9696 <bryn2009@live.co.uk>
2022-04-06 18:13:06 +0100

    reverse array method written with timings ready to plot on a graph

--- AC1/lib/arrays.rb
+++ AC1/lib/arrays.rb
@@ -18,7 +18,20 @@ class Arrays
       return a.compact
     end
 
-
+    def reverse(arr)
+      l = arr.length
+      a = []
+      start_time = Time.now
+      until l == 0
+        arr.each do |x|
+          l -= 1
+          a.insert(0, x)
+        end
+      end
+      end_time = Time.now
+      p end_time - start_time
+      return a
+    end
 
 
 end
