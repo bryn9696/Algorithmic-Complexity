d639084be1ca7eee5aec3db19cc953a04d3b622c
bryn9696 <bryn2009@live.co.uk>
2022-04-09 14:39:09 +0100

    reverse method written with tests passing in python

--- AC1/python/arrays.py
+++ AC1/python/arrays.py
@@ -1,13 +1,17 @@
 import random
 
-def shuffle(arr):
-      
+def shuffle(arr): 
       l = len(arr)
       a = []
       for i in arr:
-        print(i)
         a.insert(random.randrange(l-1), i)
         i += 1
         print(a)
-      print(a)
       return a
+
+def reverse(arr):
+  l = len(arr)
+  a = []
+  for i in arr:
+    a.insert(0, i)
+  return a
--- AC1/python/arrays_test.py
+++ AC1/python/arrays_test.py
@@ -1,9 +1,16 @@
 
 
 from python.arrays import shuffle
+from python.arrays import reverse
 
 def test_shuffle_five() -> None:
   assert shuffle([1, 2, 3, 4, 5]) != ([1, 2, 3, 4, 5])
 
 def test_shuffle_ten() -> None:
   assert shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) != ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+def test_reverse_five() -> None:
+  assert reverse([1, 2, 3, 4, 5]) == ([5, 4, 3, 2, 1])
+
+def test_reverse_ten() -> None:
+  assert reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == ([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
