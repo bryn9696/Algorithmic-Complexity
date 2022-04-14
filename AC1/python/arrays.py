ecdffb54cb258c3bae245134e55182ddcf95590f
bryn9696 <bryn2009@live.co.uk>
2022-04-14 11:29:36 +0100

    methods with time stamps printing for tests

--- AC1/python/arrays.py
+++ AC1/python/arrays.py
@@ -1,17 +1,29 @@
 import random
+from datetime import datetime
 
-def shuffle(arr): 
-      l = len(arr)
-      a = []
-      for i in arr:
-        a.insert(random.randrange(l-1), i)
-        i += 1
-        print(a)
-      return a
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+
+def shuffle(arr):
+  start = datetime.now()
+  l = len(arr)
+  a = []
+  for i in arr:
+    a.insert(random.randrange(l-1), i)
+    i += 1
+  end = datetime.now()
+  total = end-start
+  print("shuffle", total)
+  return a
 
 def reverse(arr):
+  start = datetime.now()
   l = len(arr)
   a = []
   for i in arr:
     a.insert(0, i)
+  end = datetime.now()
+  total = end-start
+  print("reverse", total)
   return a
