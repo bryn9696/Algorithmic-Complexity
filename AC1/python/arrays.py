5c77dbb44f7b6fa0ae6ddd02b256e16c15fe627f
bryn9696 <bryn2009@live.co.uk>
2022-04-25 17:06:43 +0100

    written sort method

--- AC1/python/arrays.py
+++ AC1/python/arrays.py
@@ -27,3 +27,22 @@ def reverse(arr):
   total = end-start
   print("reverse", total)
   return a
+
+def sort(arr):
+  start = datetime.now()
+  for i in range (len(arr) - 1):
+    if arr[0] > arr[0+1] == True:
+      arr.append(i)
+      arr.remove(i)
+    else:
+      arr.append(i+1)
+      arr.remove(i+1)
+
+  if arr[0] > arr[1] == True:
+    arr.append(arr[0])
+    arr.remove(arr[0])
+
+  end = datetime.now()
+  total = end-start
+  print("sort", total)
+  return arr
--- AC1/python/arrays_test.py
+++ AC1/python/arrays_test.py
@@ -1,7 +1,8 @@
 
 
-from python.arrays import shuffle
-from python.arrays import reverse
+from arrays import shuffle
+from arrays import reverse
+from arrays import sort
 
 def test_shuffle_five() -> None:
   assert shuffle([1, 2, 3, 4, 5]) != ([1, 2, 3, 4, 5])
@@ -14,3 +15,6 @@ def test_reverse_five() -> None:
 
 def test_reverse_ten() -> None:
   assert reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == ([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+
+def test_sort_five() -> None:
+  assert sort([5, 3, 1, 4, 2]) == ([1, 2, 3, 4, 5])
